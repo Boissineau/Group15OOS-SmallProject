626cb14af73245fe350d5092ca2bd731a4d8c971
jlehett <jlehett117@knights.ucf.edu>
2020-08-27 19:32:25 -0400

    Create initial project structure.

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Group 15 OOS Small Project</title>
+    <meta name="description" content="Group 15 OOS Small Project">
+    <meta name="author" content="Group 15 OOS">
+</head>
+
+<body>
+    <h5>Group 15's OOS Small Project</h5>
+    <p>This is the beginning of our project!</p>
+</body>
+
+</html>
